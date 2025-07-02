68302a77bab8c079a82dce0c285cdfa18eb294b3
akshatsingh-lohum <137285370+akshatsingh-lohum@users.noreply.github.com>
2025-07-02 23:45:00 +0530

    Pascal case and sub-heading for SoH

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -54,7 +54,7 @@ const Index = () => {
 
       <div className="flex flex-col items-center mt-12">
         <h1 className="text-5xl font-bold mb-8 text-center">
-          Real time cell capacity estimation
+          Real Time Cell Capacity Estimation
         </h1>
         {/* <div className="w-80 text-2xl text-gray-600 text-center">
           Press on the analyse button to start cell capacity estimation.
@@ -139,7 +139,8 @@ const Index = () => {
             <div className="flex flex-wrap items-center justify-center gap-12">
               <LargeMetric
                 label="SoH:"
-                value={`${resultsData.SoH.toFixed(2)}%`}
+                value={`${resultsData.SoH.toFixed(0)}%`}
+                subValue={`(±2%)`}
                 color="#22c55e"
                 size="xlarge"
               />
